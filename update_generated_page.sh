596bc86c69b1f8fb60cf45aaad830306ad9e2caa
Matthias Brandt <der.matthias.brandt@gmail.com>
2016-09-17 21:07:53 +0200

    Add script to update the site with the newly generated version.

---
+++ update_generated_page.sh
@@ -0,0 +1,11 @@
+# remove the current version of the web site
+rm -rf 404.html css/ fonts/ home/ img/ index.* js/ post/ project/ publication/ robots.txt  sitemap.xml tags/
+
+# switch to the "source" git branch
+git checkout source
+
+# copy the generate site to the repository's root folder
+cp -r gitpage/public/* .
+
+# switch to the "master" branch again
+git checkout master
